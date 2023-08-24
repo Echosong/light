966bd5b181d1452b09523dc9ab6325da6210732e
Echosong <Songfeiok@123>
2023-08-24 17:41:03 +0800

    feat(start): 加入捕捉错误，启动不了的时候可以看到原因

--- src/main/java/com/kdao/light/LightControllerApplication.java
+++ src/main/java/com/kdao/light/LightControllerApplication.java
@@ -5,11 +5,21 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 
+/**
+ * 启动文件
+ * @author 二胡子
+ */
 @EnableCaching
 @SpringBootApplication
 @EnableMybatisJpaRepositories
 public class LightControllerApplication {
     public static void main(String[] args) {
-        SpringApplication.run(LightControllerApplication.class, args);
+        try {
+            SpringApplication.run(LightControllerApplication.class, args);
+        }catch (Exception e){
+            //加入捕捉错误，启动不了的时候可以看到原因
+            e.printStackTrace();
+        }
+
     }
 }
