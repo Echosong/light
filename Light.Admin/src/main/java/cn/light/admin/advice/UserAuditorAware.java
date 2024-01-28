eb89c3f98be20348c31c7c87cb9e9a1f058e54f7
Echosong <Songfeiok@123>
2024-01-28 11:08:27 +0800

    fix: 审计不需要处理登录数据
    (cherry picked from commit c804812ca3b6fc639faa1e90ba9c7049e17bfd1b)

--- Light.Admin/src/main/java/cn/light/admin/advice/UserAuditorAware.java
+++ Light.Admin/src/main/java/cn/light/admin/advice/UserAuditorAware.java
@@ -3,17 +3,11 @@ package cn.light.admin.advice;
 
 import cn.dev33.satoken.stp.StpUtil;
 import cn.hutool.core.convert.Convert;
-import jakarta.annotation.Resource;
-import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.data.redis.core.RedisTemplate;
 
-import java.util.Arrays;
-import java.util.List;
 import java.util.Optional;
 
 /**
@@ -29,8 +23,6 @@ import java.util.Optional;
 @Slf4j
 @EnableJpaAuditing
 public class UserAuditorAware implements AuditorAware<Integer> {
-    @Resource
-    private  HttpServletRequest request;
 
     /**
      * 获取当前用户信息
@@ -40,20 +32,13 @@ public class UserAuditorAware implements AuditorAware<Integer> {
      */
     @Override
     public Optional<Integer> getCurrentAuditor() {
-        String path = request.getRequestURI();
-        //除了登录其他的都要处理
-        String loginPath = "user/login";
-        String feedBack = "feedback/save";
-        List<String> blanks = Arrays.asList(loginPath, feedBack, "file/upload");
-        if (blanks.stream().anyMatch(path::endsWith) || path.startsWith("/api")) {
-            return Optional.ofNullable(Convert.toInt(5));
-        } else {
+        try {
             return Optional.ofNullable(Convert.toInt(StpUtil.getLoginId()));
+        }catch (Exception e) {
+            log.error("获取当前用户信息异常", e);
+            return Optional.of(0);
         }
-
     }
-
-
 }
 
 
