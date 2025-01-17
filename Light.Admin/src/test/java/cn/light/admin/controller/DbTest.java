e6e10d8610f99a3eee71668b60052e2f8117afe2
Echosong <Songfeiok@123>
2025-01-17 22:27:47 +0800

    feat：增加DbTest相关依赖

--- Light.Admin/src/test/java/cn/light/admin/controller/DbTest.java
+++ Light.Admin/src/test/java/cn/light/admin/controller/DbTest.java
@@ -1,17 +1,10 @@
 package cn.light.admin.controller;
 
-import cn.light.entity.entity.SysLog;
-import cn.light.entity.mapper.LogMapper;
-import jakarta.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
-import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.util.Optional;
 
 /**
  * DbTest
